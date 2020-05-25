15b0fe29d12bef3ae511944ad1575f861057ec64
miguel jimenez <denkatalabeno@gmail.com>
2020-05-25 11:17:15 +0800

    created the concept for the array cardio

---
+++ array_cardio/array_cardio.rb
@@ -0,0 +1,74 @@
+
+# WARMUP 🏃
+
+def get_first(arr)
+  # Get the first element of the array
+end
+
+def get_last(arr)
+  # Get the last element of the array
+end
+
+def get_at_index(arr, index)
+  # Fetch an element in the array at the specified index
+end
+
+def pop_first(arr)
+  # Take the first element out of the array
+end
+
+def eliminate_repeat_values(arr)
+  # Take the first element out of the array
+end
+
+def merge_arrays(arr, other_arr)
+  # merge two arrays together (ex.)
+  # merge_arrays(["a", "b"], ["c", "d"])
+  # => ["a", "b", "c", "d"]
+end
+
+# WORKOUT 🏋
+
+def union(arr, other_arr)
+  # gets all common values in two arrays (ex.)
+  # merge_arrays(["a", "b", "c"], ["c", "z", "y"])
+  # => ["c"]
+end
+
+def non_union(arr, other_arr)
+  # gets all non-shared values in two arrays (ex.)
+  # merge_arrays(["a", "b", "c"], ["c", "z", "y"])
+  # => ["a", "b", "z", "y"]
+end
+
+def subtraction(arr, other_arr)
+  # take away values in arr that are also present in other arr (ex.)
+  # subtraction(["a", "b", "c"], ["c", "z", "y"])
+  # => ["a", "b"]
+end
+
+def all_even_at_start(arr)
+  # given an array, get all even values at the beginning of it (ex.)
+  # all_even_at_start([1,2,3,4,5,6])
+  # => [2,4,6,1,3,5]
+  # all_even_at_start([1,2,2,2,3])
+  # => [2,2,2,1,3]
+end
+
+def acronym(arr)
+  # given an array of words, return a string
+  # that makes an acronym
+  # acronym(["Grim", "Fandango"])
+  # => "G.F."
+  # acronym(["Cash", "Rules", "Everything", "Around", "Me"])
+  # => "C.R.E.A.M."
+end
+
+def stop_sign(arr)
+  # given an array of words, give all values until you find the word "STOP"
+  # (ex.)
+  # stop_sign([""])
+end
+
+
+
